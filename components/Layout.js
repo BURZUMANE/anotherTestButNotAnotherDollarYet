78328f451b1fd78b5cff8e918d6041144353c10a
Timur Zakirov <timurzakirov@Timurs-MacBook-Air.local>
2020-07-16 02:49:54 +0300

    delta

--- components/Layout.js
+++ components/Layout.js
@@ -9,13 +9,7 @@ const Layout = props => {
         <link
           rel="stylesheet"
           href="https://bootswatch.com/4/journal/bootstrap.min.css"></link>
-        {process.env.NODE_ENV !== "production" && (
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href={"/_next/static/css/styles.chunk.css?v=" + Date.now()}
-          />
-        )}
+          
       </Head>
       <Navbar />
       <div className="container">{props.children}</div>
